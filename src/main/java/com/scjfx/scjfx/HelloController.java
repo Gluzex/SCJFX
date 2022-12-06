c5a06bfd222202fc6d15998c54c0857e63196681
@40DonskoyAD <40DonskoyAD@region.cbr.ru>
2022-12-06 16:28:06 +0300

    Initial commit

---
+++ src/main/java/com/scjfx/scjfx/HelloController.java
@@ -0,0 +1,14 @@
+package com.scjfx.scjfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
